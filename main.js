bc4dc6d40288098717283587d70a7f6a707456e5
Nick DeJesus <ndejesus1227@gmail.com>
2015-11-16 15:56:01 -0500

    contact form

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('#submit').click(function() {
+
+        $.post("send.php", $("#mycontactform").serialize(), function(response) {
+            $('#success').html(response);
+            //$('#success').hide('slow');
+        });
+        return false;
+
+
+    });
+
+});
